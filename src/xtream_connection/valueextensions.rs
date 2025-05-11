4ef73c43a3c156b85f10efbf070c12c3546c8166
Brian Millham <bmillham@gmail.com>
2025-05-11 02:59:58 -0400

    Added parent_id. Changed all the _id to return i32

--- src/xtream_connection/valueextensions.rs
+++ src/xtream_connection/valueextensions.rs
@@ -1,11 +1,14 @@
 use chrono::DateTime;
 use serde_json::Value;
+use std::str::FromStr;
 
 pub trait ValueExtensions {
     fn get_name(&self) -> String;
     fn get_category_name(&self) -> &str;
-    fn get_category_id(&self) -> &str;
-    fn get_stream_id(&self) -> String;
+    fn to_i32(&self, val: &str) -> i32;
+    fn get_category_id(&self) -> i32;
+    fn get_parent_id(&self) -> i32;
+    fn get_stream_id(&self) -> i32;
     fn expires(&self) -> String;
     fn created(&self) -> String;
     fn max_connections(&self) -> i64;
@@ -13,7 +16,7 @@ pub trait ValueExtensions {
     fn status(&self) -> &str;
     fn active_cons(&self) -> &str;
     fn get_ext(&self) -> String;
-    fn get_num(&self) -> String;
+    fn get_num(&self) -> i32;
 }
 
 impl ValueExtensions for Value {
@@ -23,14 +26,20 @@ impl ValueExtensions for Value {
     fn get_category_name(&self) -> &str {
         self["category_name"].as_str().unwrap_or_default()
     }
-    fn get_category_id(&self) -> &str {
-        self["category_id"].as_str().unwrap_or("-1")
-    }
-    fn get_stream_id(&self) -> String {
-        match self["stream_id"].is_string() {
-            true => self["stream_id"].as_str().unwrap().to_string(),
-            false => self["stream_id"].as_i64().unwrap().to_string(),
+    fn to_i32(&self, val: &str) -> i32 {
+        match self[val].as_str() {
+            Some(s) => i32::from_str(s).unwrap_or(-1),
+            _ => self[val].as_i64().unwrap_or(-1) as i32,
         }
+    }
+    fn get_category_id(&self) -> i32 {
+        self.to_i32("category_id")
+    }
+    fn get_parent_id(&self) -> i32 {
+        self.to_i32("parent_id")
+    }
+    fn get_stream_id(&self) -> i32 {
+        self.to_i32("stream_id")
     }
     fn get_ext(&self) -> String {
         let x = self["container_extension"].as_str().unwrap_or_default();
@@ -39,11 +48,8 @@ impl ValueExtensions for Value {
             false => format!(".{x}"),
         }
     }
-    fn get_num(&self) -> String {
-        match self["num"].is_string() {
-            true => self["num"].as_str().unwrap().to_string(),
-            false => self["num"].as_i64().unwrap().to_string(),
-        }
+    fn get_num(&self) -> i32 {
+        self.to_i32("num")
     }
     fn expires(&self) -> String {
         let exp_ts = match self["user_info"]["exp_date"].as_str() {
